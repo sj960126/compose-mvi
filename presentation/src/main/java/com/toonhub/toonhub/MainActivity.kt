db68ba9abd1bc623749ffb2adb88448375f0f6e5
Hyunsoo Song <sj960126@naver.com>
2024-02-13 15:52:07 +0900

    build : compose activity import

--- presentation/src/main/java/com/toonhub/toonhub/MainActivity.kt
+++ presentation/src/main/java/com/toonhub/toonhub/MainActivity.kt
@@ -2,10 +2,24 @@ package com.toonhub.toonhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+        }
+    }
+
+    @Composable
+    fun Greeting(names : String){
+        Column {
+            Text(text = names)
+        }
     }
 }
